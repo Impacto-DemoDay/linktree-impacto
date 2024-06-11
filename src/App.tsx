6622871655d6003e10263dd478048d412f676e51
Gusales <dev.gussales@gmail.com>
2024-06-11 01:02:22 -0300

    linktree finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { Instagram, Linkedin } from 'lucide-react'
+
 import logo from '@/assets/logo.svg'
 import WWW from '@/assets/icones/www.svg'
-import youtube from '@/assets/icones/youtube.svg'
-import { Instagram, Linkedin } from 'lucide-react'
+import dossie from '@/assets/icones/dossie.svg'
+import Balao from '@/assets/icones/balao.svg'
+
 import { LinkButton } from './components/link-button'
 import { Card } from './components/cards'
+
 import Gabriel from '@/assets/images/gabriel-braz.jpeg'
 import Fabio from '@/assets/images/fabio.jpeg'
 import Celina from '@/assets/images/celina.jpeg'
@@ -12,8 +16,82 @@ import Helen from '@/assets/images/helen.jpeg'
 import Lucas from '@/assets/images/lucas.jpeg'
 import Kaua from '@/assets/images/kaua.jpeg'
 
+const colaboradoresImpacto = [
+  {
+    nome: "Celina Wendy",
+    image: Celina,
+    cargo: "Frontend",
+    redes: {
+      instagram: "https://www.instagram.com/wen_zegarra/",
+      github: "https://github.com/Celina22w",
+      linkedin: "https://www.linkedin.com/in/celina-lozano-981895282/"
+    }
+  },
+  {
+    nome: "Fabio Henrique",
+    image: Fabio,
+    cargo: "Backend",
+    redes: {
+      instagram: "https://www.instagram.com/henrique_fhsilva/",
+      github: "https://github.com/FH-Silva",
+      linkedin: "https://www.linkedin.com/in/fh-silva/"
+    }
+  },
+  {
+    nome: "Gabriel Braz",
+    image: Gabriel,
+    cargo: "Scrum Master e FullStack",
+    redes: {
+      github: "https://github.com/bieelbrz",
+      linkedin: "https://www.linkedin.com/in/gabbraz/"
+      }
+  },
+  {
+    nome: "Gustavo Sales",
+    image: Gustavo,
+    cargo: "Q.A e Fullstack",
+    redes: {
+      instagram: "https://www.instagram.com/devgusales/",
+      github: "https://github.com/Gusales",
+      linkedin: "https://www.linkedin.com/in/devgusales/"
+    }
+  },
+  {
+    nome: "Helen Silva",
+    image: Helen,
+    cargo: "UX/UI, Financeira e Frontend",
+    redes: {
+      instagram: "https://www.instagram.com/_aart.girl/",
+      github: "https://github.com/helenrocha19",
+      linkedin: "https://www.linkedin.com/in/helen-silva-26b176231/"
+    }
+  },
+  {
+    nome: "Kauã Dolores",
+    image: Kaua,
+    cargo: "Backend e DevOps",
+    redes: {
+      instagram: "https://www.instagram.com/doloresskaua22/",
+      github: "https://github.com/Kauadoloress",
+      linkedin: "https://www.linkedin.com/in/kaua-dolores-developer/"
+    }
+  },
+  {
+    nome: "Lucas Tomaz",
+    image: Lucas,
+    cargo: "Product Owner, UX/UI e Frontend",
+    redes: {
+      instagram: "https://www.instagram.com/_lucastomazz_/",
+      github: "https://github.com/LucasLTM",
+      linkedin: "https://www.linkedin.com/in/lucas-tomaz-monteiro-331149270/"
+    }
+  },
+]
+
 export default function App() {
   return (
+    <>
+    <div className="fixed inset-0 bg-zinc-50/35 backdrop-blur-sm" />
     <div className="absolute inset-0 flex items-center flex-col">
       <figure className="mt-10">
         <img src={logo} alt="Logo da Impacto" />
@@ -30,26 +108,33 @@ export default function App() {
           <Linkedin className="text-[#15406B] hover:opacity-70 cursor-pointer" height="28" width="28" /> 
         </a>
       </div>
-      <div className="w-2/5 mt-4 space-y-6">
+      <div className="w-[80%] md:w-2/5 mt-4 space-y-6">
         <LinkButton linkButtonIcon={WWW} linkButtonTitulo="Plataforma" linkParaRedirecionar="#" />
-        <LinkButton linkButtonIcon={youtube} linkButtonTitulo="Dossiê" linkParaRedirecionar="#" />
+        <LinkButton linkButtonIcon={dossie} linkButtonTitulo="Dossiê" linkParaRedirecionar="#" />
       </div>
-      <div className='mt-7 font-bold text-xl text-center text-[#15406B]'>
+      <div className='mt-7 font-bold text-2xl text-center text-[#15406B]'>
         <h3>Conheça a equipe Impacto</h3>
       </div>
-      <div className="mt-7">
-        <Card image={Celina} nome='Celina Wendy' alt='Foto da Celina Wendy - Front-end' />
-        <Card image={Fabio} nome='Fabio Henrique' alt='Foto do Fabio Henrique - Back-end' />
-        <Card image={Gabriel} nome='Gabriel Braz' alt='Foto do Gabriel Braz - Scrum Master' />
-        <Card image={Gustavo} nome='Gustavo Sales' alt='Foto do Gustavo Sales - Quality Assurance' />
-        <Card image={Helen} nome='Helen Silva' alt='Foto da Helen Silva - Financeira' />
-        <Card image={Kaua} nome='Kauã Dolores' alt='Foto do Kauã Dolores - Back-end' />
-        <Card image={Lucas} nome='Lucas Tomaz' alt='Foto do Lucas Tomaz - Product Owner' />
+      <div className="mt-7 flex lg:grid flex-col md:grid-cols-2 w-5/12 justify-items-center gap-y-4 place-items-center">
+      {colaboradoresImpacto.map((impactante, index) => (
+            <div
+              key={index}
+              className={`${index === colaboradoresImpacto.length - 1 && index % 2 === 0 ? 'col-span-2' : ''}`}
+            >
+              <Card
+                image={impactante.image}
+                nome={impactante.nome}
+                alt={`${impactante.nome} - ${impactante.cargo}`}
+                redes={impactante.redes}
+                cargo={impactante.cargo}
+              />
+            </div>
+          ))}
       </div>
-      <div className='w-2/5 mt-4 space-y-6'>
-      <LinkButton linkButtonIcon={WWW} linkButtonTitulo='Fale Conosco' linkParaRedirecionar='#' />
+      <div className='w-[80%] md:w-2/5 mt-4 space-y-6 pb-4'>
+        <LinkButton linkButtonIcon={Balao} linkButtonTitulo='Fale Conosco' linkParaRedirecionar='mailto:projeto.impactosocial@gmail.com' />
       </div>
     </div>
-    
+    </>
   )
 }
--- src/components/cards.tsx
+++ src/components/cards.tsx
@@ -1,16 +1,59 @@
+import InstagramIcone from '@/assets/icones/instagram.svg'
+import LinkedinIcone from '@/assets/icones/linkedin.svg'
+import GithubIcone from '@/assets/icones/github.svg'
 
 interface CardProps {
   image: string
   nome: string
   alt: string
+  cargo: string
+  redes: {
+    instagram?: string,
+    github: string,
+    linkedin: string
+  }
 }
-export function Card({ image, nome, alt }: CardProps) {
+
+const regexInstagram = /https:\/\/www\.instagram\.com\/([^/]+)\//
+const regexGithub = /https:\/\/github\.com\/([^/]+)/;
+
+export function Card({ image, nome, alt, redes, cargo }: CardProps) {
+  const instagramMatch = redes.instagram?.match(regexInstagram)
+  const githubMatch = redes.github.match(regexGithub)
+  const usuarioInstagram = (instagramMatch && instagramMatch[1]) ? instagramMatch[1] : ""
+  const usuarioGithub = (githubMatch && githubMatch[1]) ? githubMatch[1] : ""
     return(
-      <article className='w-60 p-5 bg-[#004AAD] border-[3px] border-[#001858] rounded-xl'>
-          <figure>
-            <img className='border-[3px] border-[#001858] rounded-xl' src={image} alt={alt} />
-          </figure>
-          <p className='text-center mt-5 text-white font-bold text-xl shadow-sm'>{nome}</p>
-        </article>
+      <article className='group [pespective: 1000px]'>
+        <div className="relative w-72 h-[330px] [transform-style:preserve-3d] transition-all duration-500 group-hover:[transform:rotateY(180deg)]">
+          <div className="absolute inset-0 p-5 bg-[#004AAD] border-[3px] border-[#001858] rounded-xl shadow-md">
+            <figure>
+              <img className='border-[3px] border-[#001858] rounded-xl max-h-[242px] w-full object-cover' src={image} alt={alt} />
+            </figure>
+            <p className='text-center mt-5 text-white font-bold text-xl shadow-sm'>{nome}</p>
+          </div>
+          <div className="absolute inset-0 p-5 bg-[#004AAD] border-[3px] border-[#001858] rounded-xl shadow-md [transform:rotateY(180deg)] [backface-visibility:hidden]">
+            <h4 className="text-zinc-50 font-semibold text-center text-2xl mb-12">{cargo}</h4>
+
+            <div className="flex flex-col gap-4">
+              { redes.instagram && (
+                <a href={redes.instagram} target='_blank' className="flex items-center text-xl font-semibold gap-2 text-white hover:underline hover:underline-offset-2">
+                  <img src={InstagramIcone} alt="Icone do instagram" />
+                  <p>@{usuarioInstagram}</p>
+                </a>
+              ) }
+
+              <a href={redes.linkedin} target='_blank' className="flex items-center text-xl font-semibold gap-2 text-white hover:underline hover:underline-offset-2">
+                <img src={LinkedinIcone} alt="Icone do linkedin" />
+                <p>{nome}</p>
+              </a>
+
+              <a href={redes.github} target='_blank' className="flex items-center text-xl font-semibold gap-2 text-white hover:underline hover:underline-offset-2">
+                <img src={GithubIcone} alt="Icone do linkedin" />
+                <p>{usuarioGithub}</p>
+              </a>
+            </div>
+          </div>
+        </div>
+      </article>
     )
   }
